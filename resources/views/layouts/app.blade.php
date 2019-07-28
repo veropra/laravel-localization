cb4242549fd15ec554c03cbb1e4eef956599cb42
veropra <prainoveronica@gmail.com>
2019-07-28 18:49:47 +0200

    Translated route EN/IT + navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,10 +35,15 @@
           <a class="nav-link disabled" href="#">Disabled</a>
         </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+      <ul class="navbar-nav ml-auto">
+        @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+          <li class="nav-item">
+            <a class="nav-link" rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
+              {{ $properties['native'] }}
+            </a>
+          </li>
+        @endforeach
+      </ul>
     </div>
   </nav>
   <div class="container">
